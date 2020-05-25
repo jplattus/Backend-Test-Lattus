bc9e6ca4b607cb9ab8324deb3105eee42600baf4
Jean Lattus <jplattus@gmail.com>
2020-05-24 22:24:40 -0400

    chore: app views init

--- noraslunch/views.py
+++ noraslunch/views.py
@@ -1,3 +1,17 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.template import loader, Context
+
+
+def index(request):
+    return render(request, 'noraslunch/index.html')
+
+
+def create_menu(request):
+    return render(request, 'noraslunch/create_menu.html')
+
+
+def menu_detail(request):
+    return render(request, 'noraslunch/menu_detail.html')
