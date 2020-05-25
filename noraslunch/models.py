a9fb68ee5ff857b8f14bc8777f15c7e2869ea09f
Jean Lattus <jplattus@gmail.com>
2020-05-24 22:27:03 -0400

    chore: app models init

--- noraslunch/models.py
+++ noraslunch/models.py
@@ -1,3 +1,28 @@
+import uuid
+
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    user = models.ForeignKey(User, verbose_name='usuario', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Meal(BaseModel):
+    description = models.CharField('almuerzo', max_length=200)
+
+
+class Menu(BaseModel):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    meal_day = models.DateField('día')
+
+
+class EmployeeMeal(BaseModel):
+    meal = models.ForeignKey(Meal, verbose_name='almuerzo', on_delete=models.CASCADE)
+    customization = models.CharField('especificación', max_length=200)
+
