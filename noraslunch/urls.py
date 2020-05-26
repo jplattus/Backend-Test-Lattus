4857dce19ce012ab01f3ba282dbadb77575e7f2e
Jean Lattus <jplattus@gmail.com>
2020-05-26 19:41:07 -0400

    feat: create employee meal and thanks when done

--- noraslunch/urls.py
+++ noraslunch/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('home/', views.MenuList.as_view(), name='home'),
     path('crear_menu/', views.MenuCreateView.as_view(), name='create_menu'),
-    path('menu/<str:id>/', views.MenuDetailView.as_view(), name='menu_detail'),
+    path('menu_detail/<str:id>/', views.MenuDetailView.as_view(), name='menu_detail'),
+    path('menu/<str:id>/', views.EmployeeMealCreateView.as_view(), name='menu'),
+    path('thanks/', views.thanks, name='thanks'),
 ]
--- noraslunch/views.py
+++ noraslunch/views.py
@@ -1,16 +1,14 @@
+from uuid import UUID
 
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
-from django.template import loader, Context
 from django.urls import reverse
 from django.utils import timezone
 from django.views.generic import CreateView, ListView, DetailView
 
-from noraslunch.forms import MenuForm, MealFormset
+from noraslunch.forms import MealFormset, EmployeeMealForm
 from noraslunch.models import Menu, EmployeeMeal
 
 
@@ -69,3 +67,24 @@ class MenuDetailView(LoginRequiredMixin, DetailView):
         return context
 
 
+class EmployeeMealCreateView(LoginRequiredMixin, CreateView):
+    model = EmployeeMeal
+    template_name = "noraslunch/create_employee_meal.html"
+    form_class = EmployeeMealForm
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['menu_id'] = self.kwargs.get('id')
+        return kwargs
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        form.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse("thanks")
+
+
+def thanks(request):
+    return render(request, 'noraslunch/thanks.html')
